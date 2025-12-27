ee4bb4519b2740f9ff05b55c724f97c89646ea22
ThaoMy <ngothaomyoppo1912@gmail.com>
2025-12-27 16:23:11 +0700

    Add file assets and config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+HOST = '127.0.0.1'          
+PORT = 65432              
+
+CHOICES = ['rock', 'paper', 'scissors']                    
+
+CHOICES_VN = {                                           
+    'rock': 'Búa',
+    'paper': 'Bao',
+    'scissors': 'Kéo'
+}
+
+VALID_MODES = ['1', '2']                                   
+
+APP_NAME = "Búa - Bao - Kéo Multiplayer"
+APP_VERSION = "1.5.0"                                    
