5c57a9da914fbc06425c8afa9eedd949704d4628
thusuong12 <thusuong1319@gmail.com>
2026-01-21 19:18:45 +0700

    Implement SocketClient class for socket communication
    add client socket layer and protocol receiver

--- client/client.py
+++ client/client.py
@@ -6,3 +6,33 @@ import sys
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if BASE_DIR not in sys.path:
     sys.path.append(BASE_DIR)
+
+
+from config import SERVER_HOST, PORT
+from utils import recv_line
+
+class SocketClient:
+    def __init__(self):
+        self.sock = None
+
+    def connect(self):
+        self.sock = socket.socket()
+        self.sock.connect((SERVER_HOST, PORT))
+        return True
+
+    def send_line(self, msg):
+        self.sock.sendall((msg + "|").encode())
+
+    def recv_message(self):
+        line = recv_line(self.sock)
+        if not line:
+            return None, None
+        if ":" in line:
+            return line.split(":", 1)
+        return line, ""
+
+    def close(self):
+        try:
+            self.sock.close()
+        except:
+            pass
